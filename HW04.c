fdc708f63534f7b22e99765a76645707b5efb4ae
heathcliffYang <ginny0922fc2@gmail.com>
2018-11-08 09:19:01 +0800

    Correct classification judge

--- HW04.c
+++ HW04.c
@@ -53,24 +53,28 @@ int main(int argc, char **argv)
         /* initial w */
         matrix *w = create_matrix(2, 1);
         int num_of_I_error = 0, num_of_II_error = 0, i1 = 0, i2 = 0, iter = 0;
-        double learning_rate = 0.5;
+        double learning_rate = 1;
         /* Steepest gradient descent */
         do
         {
             iter++;
-            //printf("weights:\n");
-            //print_matrix(w);
+            printf("weights:\n");
+            print_matrix(w);
             /* -- Collect type I and type II error */
             printf("Collect misclassified data...\n");
             matrix *classify_result1 = multi_matrix(D1, w); //  if D1 == 1, type I error  (correct is 0)
             matrix *classify_result2 = multi_matrix(D2, w); //  if D2 == 0, type II error (correct is 1)
+            printf("Classify result1\n");
+            print_matrix(classify_result1);
+            printf("Classify result2\n");
+            print_matrix(classify_result2);
             /* TODO: append these errors into a matrix */
             num_of_I_error = 0, num_of_II_error = 0, i1 = 0, i2 = 0;
             for (int i = 0; i < num_of_data; i++)
             {
-                if (classify_result1->data[i][0] >= 0.5)
+                if (1 / (1 + exp(-classify_result1->data[i][0])) >= 0.5)
                     num_of_I_error++;
-                if (classify_result2->data[i][0] < 0.5)
+                if (1 / (1 + exp(-classify_result2->data[i][0])) < 0.5)
                     num_of_II_error++;
             }
             printf("Type I error: %d\nType II error: %d\n", num_of_I_error, num_of_II_error);
@@ -83,13 +87,13 @@ int main(int argc, char **argv)
             matrix *II_error = create_matrix(num_of_II_error, 2);
             for (int i = 0; i < num_of_data; i++)
             {
-                if (classify_result1->data[i][0] >= 0.5)
+                if (1 / (1 + exp(-classify_result1->data[i][0])) >= 0.5)
                 {
                     I_error->data[i1][0] = D1->data[i][0];
                     I_error->data[i1][1] = D1->data[i][1];
                     i1++;
                 }
-                if (classify_result2->data[i][0] < 0.5)
+                if (1 / (1 + exp(-classify_result2->data[i][0])) < 0.5)
                 {
                     II_error->data[i2][0] = D2->data[i][0];
                     II_error->data[i2][1] = D2->data[i][1];
@@ -104,10 +108,16 @@ int main(int argc, char **argv)
             matrix *II_error_T = tran_matrix(II_error);
             matrix *exp_part_I = create_matrix(num_of_I_error, 1);
             matrix *exp_part_II = create_matrix(num_of_II_error, 1);
+            printf("I_error\n");
+            print_matrix(I_error);
+            printf("II_error\n");
+            print_matrix(II_error);
             for (int i = 0; i < num_of_I_error; i++)
             {
-                exp_part_I->data[i][0] = -learning_rate * ((1 / (1 + exp(I_error_W->data[i][0]))) - 0);
+                exp_part_I->data[i][0] = /*-learning_rate **/ ((1 / (1 + exp(-I_error_W->data[i][0]))) - 0);
             }
+            printf("exp I\n");
+            print_matrix(exp_part_I);
             matrix *gradient_f_I = multi_matrix(I_error_T, exp_part_I);
             add_assign_matrix(gradient_f_I, w, 0, true);
             printf("gradient_f_I\n");
@@ -115,8 +125,10 @@ int main(int argc, char **argv)
 
             for (int i = 0; i < num_of_II_error; i++)
             {
-                exp_part_II->data[i][0] = learning_rate * ((1 / (1 + exp(II_error_W->data[i][0]))) - 1);
+                exp_part_II->data[i][0] = /*learning_rate **/ ((1 / (1 + exp(-II_error_W->data[i][0]))) - 1);
             }
+            printf("exp II\n");
+            print_matrix(exp_part_II);
             matrix *gradient_f_II = multi_matrix(II_error_T, exp_part_II);
             add_assign_matrix(gradient_f_II, w, 0, true);
             printf("gradient_f_II\n");
